2d2657e639bda3101c0fa4d229a25ea211a5075e
VictorDiego32 <victoralocard@gmail.com>
2026-05-21 19:50:45 -0300

    import

---
+++ bancodedados.py
@@ -0,0 +1,8 @@
+import sqlite3
+import pandas as pd
+import requests
+import flask   
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+pd.api.extensions.register_dataframe_accessor("banco_de_dados")
